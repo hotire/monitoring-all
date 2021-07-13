a088c3ca0b9eb8cc84f877745e91db443489d2ed
hotire <gngh0101@gmail.com>
2021-07-14 00:29:29 +0900

    Add spring-actuator

---
+++ spring-actuator/src/test/kotlin/com/github/hotire/springactuator/SpringActuatorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.hotire.springactuator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
