5fc62042d36dceb8996a6720cb7d475defe75c33
hotire <gngh0101@gmail.com>
2022-03-09 17:38:51 +0900

    Add kotlin-logging:

--- spring-actuator/build.gradle.kts
+++ spring-actuator/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("net.logstash.logback:logstash-logback-encoder:6.6")
+    implementation("io.github.microutils:kotlin-logging:1.12.5")
 }
 
 dependencyManagement {
